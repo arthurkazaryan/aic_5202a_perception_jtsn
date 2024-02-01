cb743ecdf057468359b0df2171adbb75d75a124d
arthurkazaryan <arthur.kazaryan@yahoo.com>
2024-02-01 20:45:54 +0100

    Added path, trespassing detections and people counting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import glob
+import math
 from collections import deque
 
 import cv2
@@ -6,16 +7,16 @@ import numpy as np
 from argparse import ArgumentParser
 from ultralytics import YOLO
 
-from utils import generate_rgb_color, TRACKING_LEN
+from utils import generate_rgb_color, draw_trespass_region, TRACKING_LEN, is_trespassing
 
-# tracking = {0: deque([], 3)}
+
 tracking = {}
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--model", type=str, default="yolov8n", help="name of the model")
-    parser.add_argument("--folder_path", type=str, default="dataset/test_sequence_2_normalized", help="path to a dataset")
+    parser.add_argument("--folder_path", type=str, default="dataset/test_sequence_1_normalized", help="path to a dataset")
     parser.add_argument("--export", type=str, default="", help="export path")
 
     args = parser.parse_args()
@@ -31,40 +32,49 @@ if __name__ == "__main__":
         frame = cv2.resize(frame, [640, 640], interpolation=cv2.INTER_AREA)
         # frame = cv2.normalize(frame, None, 0, 1.0, cv2.NORM_MINMAX, dtype=cv2.CV_32F) * 255
 
-        results = model.predict(frame)
+        results = model.predict(frame, verbose=False)
+        frame = draw_trespass_region(frame)
+        people = 0
+        trespassing = 0
+        for r in results:
+            for xyxy in r.boxes.xyxy.numpy():
+                people += 1
+                x_min, y_min, x_max, y_max = xyxy
+                x = (x_max - x_min) / 2 + x_min
+                y = (y_max - y_min) / 2 + y_min
+                appended = False
+                min_distance = float("inf")
+                min_key = None
+                for key, track in tracking.items():
+                    current_point = np.array([x, y])
+                    track_data = np.array(track)[-1]
+                    distance = math.hypot(x - track_data[0], y - track_data[1])
+                    if distance < min_distance:
+                        min_distance = distance
+                        min_key = key
+                        min_track = track
+                if min_distance < 20:
+                    tracking[min_key].append([int(x), int(y)])
+                    appended = True
+                    if is_trespassing(x, y):
+                        trespassing += 1
+                if not appended:
+                    key = generate_rgb_color()
+                    track = deque([[int(x), int(y)]], TRACKING_LEN)
+                    tracking[key] = track
+                    min_key = key
+                    min_track = track
+                    if is_trespassing(x, y):
+                        trespassing += 1
+                frame = cv2.rectangle(frame, (int(x_min), int(y_min)), (int(x_max), int(y_max)), min_key, 4)
+                if len(track) > 1:
+                    points = np.array(list(min_track), np.int32).reshape((-1, 1, 2))
+                    frame = cv2.polylines(frame, [points], False, min_key, 5)
+        txt_col = (0, 255, 0) if trespassing == 0 else (0, 0, 255)
+        frame = cv2.putText(frame, f'People: {people}', (25, 600), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        frame = cv2.putText(frame, f'Trespassing: {trespassing}', (400, 600), cv2.FONT_HERSHEY_SIMPLEX, 1, txt_col, 2, cv2.LINE_AA)
 
-        if len(results) > 0:
-            for r in results:
-                for xyxy in r.boxes.xyxy.numpy():
-                    x_min, y_min, x_max, y_max = xyxy
-                    x = (x_max - x_min) / 2 + x_min
-                    y = (y_max - y_min) / 2 + y_min
-                    appended = False
-                    min_distance = float("inf")
-                    min_key = None
-                    for key, track in tracking.items():
-                        current_point = np.array([x, y])
-                        mean_track_data = np.array(track).mean(axis=0)
-                        distance = np.linalg.norm(current_point - mean_track_data)
-                        if distance < min_distance:
-                            min_distance = distance
-                            min_key = key
-                        # if abs(x - track[0][0]) < 80 and abs(y - track[0][1]) < 80:
-                        #     tracking[key].append([int(x), int(y)])
-                        #     appended = True
-                        #     break
-                    if min_distance < 50:
-                        tracking[min_key].append([int(x), int(y)])
-                        appended = True
-                    if not appended:
-                        key = generate_rgb_color()
-                        track = deque([[int(x), int(y)]], TRACKING_LEN)
-                        tracking[key] = track
-                    # res_plotted = r.plot()  # allows you to retrieve the prediction results
-                    frame = cv2.rectangle(frame, (int(x_min), int(y_min)), (int(x_max), int(y_max)), key, 4)
-                    if len(track) > 1:
-                        points = np.array(list(track), np.int32).reshape((-1, 1, 2))
-                        frame = cv2.polylines(frame, [points], False, key, 5)
+        print(f'Path: {filename}, People: {people}, Trespassing: {trespassing}')
         cv2.imshow("Frame", frame)
         if args.export:
             video_writer.write(frame)
